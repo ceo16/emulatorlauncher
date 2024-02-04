973c8627c9034ef207bebc2e9b73d66c9a8c7049
ceo16 <ceo16@libero.it>
2024-02-04 10:10:35 +0100

    Update EpicClient.cs

--- EmulatorLauncher.Common/Launchers/EpicClient.cs
+++ EmulatorLauncher.Common/Launchers/EpicClient.cs
@@ -1,37 +1,144 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 
-namespace EpicLibrary
+namespace EmulatorLauncher.Common.Launchers.Epic
+{
+    public class EpicClient
     {
-        private static readonly ILogger logger = LogManager.GetLogger();
+        private static readonly string AllUsersPath = Path.Combine(Environment.ExpandEnvironmentVariables("%PROGRAMDATA%"), "Epic");
+        private static readonly string LauncherExePath = GetExecutablePath();
+        private static readonly string MetadataPath = GetMetadataPath();
 
-        public override string Icon => EpicLauncher.Icon;
+        public static bool IsInstalled => File.Exists(LauncherExePath);
 
-        public override bool IsInstalled => EpicLauncher.IsInstalled;
-
-        public override void Open()
+        public static void Open()
         {
-            EpicLauncher.StartClient();
+            Process.Start(LauncherExePath);
         }
 
-        public override void Shutdown()
+        public static void Shutdown()
         {
             var mainProc = Process.GetProcessesByName("EpicGamesLauncher").FirstOrDefault();
             if (mainProc == null)
             {
-                logger.Info("Epic client is no longer running, no need to shut it down.");
                 return;
             }
 
-            var procRes = ProcessStarter.StartProcessWait(CmdLineTools.TaskKill, $"/f /pid {mainProc.Id}", null, out var stdOut, out var stdErr);
-            if (procRes != 0)
-            {
-                logger.Error($"Failed to close Epic client: {procRes}, {stdErr}");
+            var procRes = ProcessStarter.StartProcessWait(CmdLineTools.TaskKill, <span class="math-inline">"/f /pid \{mainProc\.Id\}", null, out var stdOut, out var stdErr\);
+if \(procRes \!\= 0\)
+\{
+Console\.WriteLine\(</span>"Errore durante la chiusura del client Epic: {procRes}, {stdErr}");
             }
         }
-    }
 
+        public static string GetIcon()
+        {
+            return Path.Combine(Path.GetDirectoryName(LauncherExePath), "Engine\\Binaries\\Win64\\UE4Editor-Cmd.exe");
+        }
+
+        public static LauncherGameInfo[] GetInstalledGames()
+        {
+            if (!IsInstalled)
+            {
+                return new LauncherGameInfo[0];
+            }
+
+            var appList = GetInstalledAppList();
+            var manifests = GetInstalledManifests();
+
+            if (appList == null || manifests == null)
+            {
+                return new LauncherGameInfo[0];
+            }
+
+            var games = new List<LauncherGameInfo>();
+
+            foreach (var app in appList)
+            {
+                if (app.AppName.StartsWith("UE_"))
+                {
+                    continue;
+                }
+
+                var manifest = manifests.FirstOrDefault(a => a.AppName == app.AppName);
+                if (manifest == null)
+                {
+                    continue;
+                }
+
+                // Skip DLCs
+                if (manifest.AppName != manifest.MainGameAppName)
+                {
+                    continue;
+                }
+
+                // Skip Plugins
+                if (manifest.AppCategories != null && manifest.AppCategories.Any(a => a == "plugins" || a == "plugins/engine"))
+                {
+                    continue;
+                }
+
+                var gameName = manifest.DisplayName ?? Path.GetFileName(app.InstallLocation);
+                var installLocation = manifest.InstallLocation ?? app.InstallLocation;
+
+                if (string.IsNullOrEmpty(installLocation))
+                {
+                    continue;
+                }
+
+                games.Add(new LauncherGameInfo
+                {
+                    Id = app.AppName,
+                    Name = gameName,
+                    LauncherUrl = string.Format(GameLaunchUrl, manifest.AppName),
+                    InstallDirectory = Path.GetFullPath(installLocation),
+                    ExecutableName = manifest.LaunchExecutable,
+                    Launcher = GameLauncherType.Epic
+                });
+            }
+
+            return games.ToArray();
+        }
+
+        public static string GetEpicGameExecutableName(Uri uri)
+        {
+            var shorturl = uri.LocalPath.ExtractString("/", ":");
+
+            if (string.IsNullOrEmpty(MetadataPath))
+            {
+                throw new ApplicationException("Impossibile trovare la cartella dei metadati di Epic Games.");
+            }
+
+            if (!Directory.Exists(MetadataPath))
+            {
+                throw new ApplicationException("Impossibile trovare la cartella dei metadati di Epic Games.");
+            }
+
+            foreach (var manifest in GetInstalledManifests())
+            {
+                if (shorturl.Equals(manifest.CatalogNamespace))
+                {
+                    return manifest.LaunchExecutable;
+                }
+            }
+
+            throw new ApplicationException("Il gioco non è installato.");
+        }
+
+        private static List<LauncherInstalled.InstalledApp> GetInstalledAppList()
+        {
+            var installListPath = Path.Combine(AllUsersPath, "UnrealEngineLauncher", "LauncherInstalled.dat");
+
+            if (!File.Exists(installListPath))
+            {
+                return new List<LauncherInstalled.InstalledApp>();
+            }
+
+            var list = JsonConvert.DeserializeObject<
