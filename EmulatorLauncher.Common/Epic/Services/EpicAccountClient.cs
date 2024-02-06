3a6e4a9c389adb1c7bbbaf455162ca058144a817
ceo16 <ceo16@libero.it>
2024-02-06 11:44:55 +0100

    Update EpicAccountClient.cs

--- EmulatorLauncher.Common/Epic/Services/EpicAccountClient.cs
+++ EmulatorLauncher.Common/Epic/Services/EpicAccountClient.cs
@@ -6,7 +6,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Security.Principal;
 using System.Text;
 using System.Text.RegularExpressions;
