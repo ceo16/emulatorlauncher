ce28a93fc3c83c6d152dc052eeb1ac73bbfc2c1c
ceo16 <ceo16@libero.it>
2024-02-06 12:58:58 +0100

    Update EpicGameController.cs

--- EmulatorLauncher.Common/Epic/EpicGameController.cs
+++ EmulatorLauncher.Common/Epic/EpicGameController.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace EpicLibrary
+namespace EmulatorLauncher.Common.Launchers
 {
     public class EpicInstallController : InstallController
     {
