c646a27b0169b7a4138611189193f0a4039cabec
ceo16 <ceo16@libero.it>
2024-02-06 13:13:20 +0100

    Update EpicClient.cs

--- EmulatorLauncher.Common/Epic/EpicClient.cs
+++ EmulatorLauncher.Common/Epic/EpicClient.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace EmulatorLauncher.Common.Launchers.Epic
+namespace EpicLibrary
 {
     public class EpicClient : LibraryClient
     {
