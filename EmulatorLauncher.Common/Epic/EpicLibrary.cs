c713ddb473490c48047b743f49f9a726bcd0aa37
ceo16 <ceo16@libero.it>
2024-02-06 13:03:24 +0100

    Update EpicLibrary.cs

--- EmulatorLauncher.Common/Epic/EpicLibrary.cs
+++ EmulatorLauncher.Common/Epic/EpicLibrary.cs
@@ -1,267 +1,207 @@
-﻿using EpicLibrary.Models;
-using EpicLibrary.Services;
-using EmulatorLauncher.Common;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Controls;
+using System.Runtime.Serialization;
+using System.IO;
+using EmulatorLauncher.Common.FileFormats;
+using Microsoft.Win32;
+using EmulatorLauncher.Common.Launchers.Epic;
 
 namespace EmulatorLauncher.Common.Launchers
 {
-    [LoadPlugin]
-    public class EpicLibrary : LibraryPluginBase<EpicLibrarySettingsViewModel>
+    public class EpicLibrary
     {
-        private static readonly ILogger logger = LogManager.GetLogger();
-        internal readonly string TokensPath;
+        const string GameLaunchUrl = @"com.epicgames.launcher://apps/{0}?action=launch&silent=true";
 
-        public EpicLibrary(IPlayniteAPI api) : base(
-            "Epic",
-            Guid.Parse("00000002-DBD1-46C6-B5D0-B1BA559D10E4"),
-            new LibraryPluginProperties { CanShutdownClient = true, HasSettings = true },
-            new EpicClient(),
-            EpicLauncher.Icon,
-            (_) => new EpicLibrarySettingsView(),
-            api)
+        public static string GetEpicGameExecutableName(Uri uri)
         {
-            SettingsViewModel = new EpicLibrarySettingsViewModel(this, api);
-            TokensPath = Path.Combine(GetPluginUserDataPath(), "tokens.json");
+            string shorturl = uri.LocalPath.ExtractString("/", ":");
+
+            var modSdkMetadataDir = GetMetadataPath();
+            if (modSdkMetadataDir != null)
+            {
+                string manifestPath = modSdkMetadataDir.ToString();
+
+                string gameExecutable = null;
+
+                if (Directory.Exists(manifestPath))
+                {
+                    foreach (var manifest in GetInstalledManifests())
+                    {
+                        if (shorturl.Equals(manifest.CatalogNamespace))
+                        {
+                            gameExecutable = manifest.LaunchExecutable;
+                            break;
+                        }
+                    }
+                }
+
+                if (gameExecutable == null)
+                    throw new ApplicationException("There is a problem: The Game is not installed");
+
+                return Path.GetFileNameWithoutExtension(gameExecutable);
+            }
+
+            throw new ApplicationException("There is a problem: Epic Launcher is not installed");
         }
 
-        internal Dictionary<string, GameMetadata> GetInstalledGames()
+        public static LauncherGameInfo[] GetInstalledGames()
         {
-            var games = new Dictionary<string, GameMetadata>();
-            var appList = EpicLauncher.GetInstalledAppList();
-            var manifests = EpicLauncher.GetInstalledManifests();
+            var games = new List<LauncherGameInfo>();
+
+            if (!IsInstalled)
+                return games.ToArray();
+
+            var appList = GetInstalledAppList();
+            var manifests = GetInstalledManifests();
+
+            if (appList == null || manifests == null)
+                return games.ToArray();
 
             foreach (var app in appList)
             {
                 if (app.AppName.StartsWith("UE_"))
-                {
                     continue;
-                }
 
                 var manifest = manifests.FirstOrDefault(a => a.AppName == app.AppName);
                 if (manifest == null)
-                {
                     continue;
-                }
 
-                // DLC
+                // Skip DLCs
                 if (manifest.AppName != manifest.MainGameAppName)
-                {
                     continue;
-                }
 
-                // UE plugins
-                if (manifest.AppCategories?.Any(a => a == "plugins" || a == "plugins/engine") == true)
-                {
+                // Skip Plugins
+                if (manifest.AppCategories == null || manifest.AppCategories.Any(a => a == "plugins" || a == "plugins/engine"))
                     continue;
-                }
 
-                var gameName = manifest?.DisplayName ?? Path.GetFileName(app.InstallLocation);
-                var installLocation = manifest?.InstallLocation ?? app.InstallLocation;
-                if (installLocation.IsNullOrEmpty())
-                {
+                var gameName = manifest.DisplayName ?? Path.GetFileName(app.InstallLocation);
+
+                var installLocation = manifest.InstallLocation ?? app.InstallLocation;
+                if (string.IsNullOrEmpty(installLocation))
                     continue;
-                }
 
-                installLocation = Paths.FixSeparators(installLocation);
-                if (!Directory.Exists(installLocation))
+                var game = new LauncherGameInfo()
                 {
-                    logger.Error($"Epic game {gameName} installation directory {installLocation} not detected.");
-                    continue;
-                }
-
-                var game = new GameMetadata()
-                {
-                    Source = new MetadataNameProperty("Epic"),
-                    GameId = app.AppName,
+                    Id = app.AppName,
                     Name = gameName,
-                    InstallDirectory = installLocation,
-                    IsInstalled = true,
-                    Platforms = new HashSet<MetadataProperty> { new MetadataSpecProperty("pc_windows") }
+                    LauncherUrl = string.Format(GameLaunchUrl, manifest.AppName),
+                    InstallDirectory = Path.GetFullPath(installLocation),
+                    ExecutableName = manifest.LaunchExecutable,
+                    Launcher = GameLauncherType.Epic
                 };
 
-                game.Name = game.Name.RemoveTrademarks();
-                games.Add(game.GameId, game);
+                games.Add(game);
             }
 
-            return games;
+            return games.ToArray();
         }
 
-        internal List<GameMetadata> GetLibraryGames(CancellationToken cancelToken)
+
+        static string AllUsersPath { get { return Path.Combine(Environment.ExpandEnvironmentVariables("%PROGRAMDATA%"), "Epic"); } }
+
+        public static bool IsInstalled
         {
-            var cacheDir = GetCachePath("catalogcache");
-            var games = new List<GameMetadata>();
-            var accountApi = new EpicAccountClient(PlayniteApi, TokensPath);
-            var assets = accountApi.GetAssets();
-            if (!assets?.Any() == true)
+            get
             {
-                Logger.Warn("Found no assets on Epic accounts.");
+                return File.Exists(GetExecutablePath());
             }
-
-            var playtimeItems = accountApi.GetPlaytimeItems();
-            foreach (var gameAsset in assets.Where(a => a.@namespace != "ue"))
-            {
-                if (cancelToken.IsCancellationRequested)
-                {
-                    break;
-                }
-
-                var cacheFile = Paths.GetSafePathName($"{gameAsset.@namespace}_{gameAsset.catalogItemId}_{gameAsset.buildVersion}.json");
-                cacheFile = Path.Combine(cacheDir, cacheFile);
-                var catalogItem = accountApi.GetCatalogItem(gameAsset.@namespace, gameAsset.catalogItemId, cacheFile);
-                if (catalogItem?.categories?.Any(a => a.path == "applications") != true)
-                {
-                    continue;
-                }
-
-                if (catalogItem?.categories?.Any(a => a.path == "dlc") == true)
-                {
-                    continue;
-                }
-
-                var newGame = new GameMetadata
-                {
-                    Source = new MetadataNameProperty("Epic"),
-                    GameId = gameAsset.appName,
-                    Name = catalogItem.title.RemoveTrademarks(),
-                    Platforms = new HashSet<MetadataProperty> { new MetadataSpecProperty("pc_windows") }
-                };
-
-                var playtimeItem = playtimeItems?.FirstOrDefault(x => x.artifactId == gameAsset.appName);
-                if (playtimeItem != null)
-                {
-                    newGame.Playtime = playtimeItem.totalTime;
-                }
-
-                games.Add(newGame);
-            }
-
-            return games;
         }
 
-        public override IEnumerable<GameMetadata> GetGames(LibraryGetGamesArgs args)
+        static string GetExecutablePath()
         {
-            var allGames = new List<GameMetadata>();
-            var installedGames = new Dictionary<string, GameMetadata>();
-            Exception importError = null;
+            var modSdkMetadataDir = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Epic Games\\EOS", "ModSdkCommand", null);
+            if (modSdkMetadataDir != null)
+                return modSdkMetadataDir.ToString();
 
-            if (SettingsViewModel.Settings.ImportInstalledGames)
+            return null;
+        }
+
+        static string GetMetadataPath()
+        {
+            var modSdkMetadataDir = Registry.GetValue("HKEY_CURRENT_USER\\Software\\Epic Games\\EOS", "ModSdkMetadataDir", null);
+            if (modSdkMetadataDir != null)
+                return modSdkMetadataDir.ToString();
+
+            return null;
+        }
+
+        static List<LauncherInstalled.InstalledApp> GetInstalledAppList()
+        {
+            var installListPath = Path.Combine(AllUsersPath, "UnrealEngineLauncher", "LauncherInstalled.dat");
+            if (!File.Exists(installListPath))
+                return new List<LauncherInstalled.InstalledApp>();
+
+            var list = JsonSerializer.DeserializeString<LauncherInstalled>(File.ReadAllText(installListPath));
+            return list.InstallationList;
+        }
+
+        static IEnumerable<EpicGame> GetInstalledManifests()
+        {
+            var installListPath = GetMetadataPath();
+            if (Directory.Exists(installListPath))
             {
-                try
+                foreach (var manFile in Directory.GetFiles(installListPath, "*.item"))
                 {
-                    installedGames = GetInstalledGames();
-                    Logger.Debug($"Found {installedGames.Count} installed Epic games.");
-                    allGames.AddRange(installedGames.Values.ToList());
-                }
-                catch (Exception e)
-                {
-                    Logger.Error(e, "Failed to import installed Epic games.");
-                    importError = e;
-                }
-            }
+                    EpicGame manifest = null;
 
-            if (SettingsViewModel.Settings.ConnectAccount)
-            {
-                try
-                {
-                    var libraryGames = GetLibraryGames(args.CancelToken);
-                    Logger.Debug($"Found {libraryGames.Count} library Epic games.");
+                    try { manifest = JsonSerializer.DeserializeString<EpicGame>(File.ReadAllText(manFile)); }
+                    catch { }
 
-                    if (!SettingsViewModel.Settings.ImportUninstalledGames)
-                    {
-                        libraryGames = libraryGames.Where(lg => installedGames.ContainsKey(lg.GameId)).ToList();
-                    }
-
-                    foreach (var game in libraryGames)
-                    {
-                        if (installedGames.TryGetValue(game.GameId, out var installed))
-                        {
-                            installed.Playtime = game.Playtime;
-                            installed.LastActivity = game.LastActivity;
-                            installed.Name = game.Name;
-                        }
-                        else
-                        {
-                            allGames.Add(game);
-                        }
-                    }
-                }
-                catch (Exception e)
-                {
-                    Logger.Error(e, "Failed to import linked account Epic games details.");
-                    importError = e;
+                    if (manifest != null)
+                        yield return manifest;
                 }
             }
-
-            if (importError != null)
-            {
-                PlayniteApi.Notifications.Add(new NotificationMessage(
-                    ImportErrorMessageId,
-                    string.Format(PlayniteApi.Resources.GetString("LOCLibraryImportError"), Name) +
-                    System.Environment.NewLine + importError.Message,
-                    NotificationType.Error,
-                    () => OpenSettingsView()));
-            }
-            else
-            {
-                PlayniteApi.Notifications.Remove(ImportErrorMessageId);
-            }
-
-            return allGames;
         }
 
-        public string GetCachePath(string dirName)
+    }    
+}
+
+namespace EmulatorLauncher.Common.Launchers.Epic
+{
+    [DataContract]
+    public class LauncherInstalled
+    {
+        [DataContract]
+        public class InstalledApp
         {
-            return Path.Combine(GetPluginUserDataPath(), dirName);
+            [DataMember]
+            public string InstallLocation { get; set; }
+            [DataMember]
+            public string AppName { get; set; }
+            [DataMember]
+            public long AppID { get; set; }
+            [DataMember]
+            public string AppVersion { get; set; }
         }
 
-        public override IEnumerable<InstallController> GetInstallActions(GetInstallActionsArgs args)
-        {
-            if (args.Game.PluginId != Id)
-            {
-                yield break;
-            }
+        [DataMember]
+        public List<InstalledApp> InstallationList { get; set; }
+    }
 
-            yield return new EpicInstallController(args.Game);
-        }
+    [DataContract]
+    public class EpicGame
+    {
+        [DataMember]
+        public string AppName { get; set; }
 
-        public override IEnumerable<UninstallController> GetUninstallActions(GetUninstallActionsArgs args)
-        {
-            if (args.Game.PluginId != Id)
-            {
-                yield break;
-            }
+        [DataMember]
+        public string CatalogNamespace { get; set; }
 
-            yield return new EpicUninstallController(args.Game);
-        }
+        [DataMember]
+        public string LaunchExecutable { get; set; }
 
-        public override IEnumerable<PlayController> GetPlayActions(GetPlayActionsArgs args)
-        {
-            if (args.Game.PluginId != Id)
-            {
-                yield break;
-            }
+        [DataMember]
+        public string InstallLocation;
 
-            yield return new AutomaticPlayController(args.Game)
-            {
-                Type = AutomaticPlayActionType.Url,
-                TrackingMode = TrackingMode.Directory,
-                TrackingPath = args.Game.InstallDirectory,
-                Path = string.Format(EpicLauncher.GameLaunchUrlMask, args.Game.GameId),
-                Name = ResourceProvider.GetString(LOC.EpicStartUsingClient).Format("Epic")
-            };
-        }
+        [DataMember]
+        public string MainGameAppName;
 
-        public override LibraryMetadataProvider GetMetadataDownloader()
-        {
-            return new EpicMetadataProvider(PlayniteApi);
-        }
+        [DataMember]
+        public string DisplayName;
+
+        [DataMember]
+        public List<string> AppCategories { get; set; }
     }
 }
