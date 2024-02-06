8e292bdaa3a3cb0cdbc306d827bcbe28e01a36e3
ceo16 <ceo16@libero.it>
2024-02-06 09:53:03 +0100

    Update EpicLauncher.Generator.cs

--- emulatorLauncher/Generators/EpicLauncher.Generator.cs
+++ emulatorLauncher/Generators/EpicLauncher.Generator.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Emulatorlauncher.Common.Epic.Models;
 using EmulatorLauncher.Common;
+EmulatorLauncher.Common.Launchers
 
 namespace EpicLibrary
 {
