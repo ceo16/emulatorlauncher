0a433e45b9acd7e995c3aa7a1efb52acfcfd8a60
ceo16 <ceo16@libero.it>
2024-02-06 11:21:10 +0100

    Update EpicLibrarySettingsViewModel.cs

--- EmulatorLauncher.Common/Epic/EpicLibrarySettingsViewModel.cs
+++ EmulatorLauncher.Common/Epic/EpicLibrarySettingsViewModel.cs
@@ -1,7 +1,5 @@
 ﻿using EpicLibrary.Services;
-using Playnite;
-using Playnite.SDK;
-using Playnite.Commands;
+using EmulatorLauncher;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,7 +10,6 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Media;
-using Playnite.SDK.Data;
 
 namespace EpicLibrary
 {
